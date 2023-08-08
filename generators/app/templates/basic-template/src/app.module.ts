01512fcc088cf888ec1180e23feb1414566c7584
Mostafa Sissi <mostafasissi@gmail.com>
2023-08-08 20:19:13 +0100

    add config module as global module

--- generators/app/templates/basic-template/src/app.module.ts
+++ generators/app/templates/basic-template/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal : true
+  }),], // Configuration globale du module Config
   controllers: [],
   providers: [],
 })
