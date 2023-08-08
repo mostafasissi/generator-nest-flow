e816a14ffa12dd354da57a1466e933d57530bae6
Mostafa Sissi <mostafasissi@gmail.com>
2023-08-08 18:20:48 +0100

    generate basic nestjs template : question (description , packageManager)

--- generators/app/steps/prompting.js
+++ generators/app/steps/prompting.js
@@ -2,7 +2,17 @@ const questions = require('../questions');
 module.exports = {
     askApp() {
         return this.prompt(questions.app).then(answers => {
-            this.answers = answers ;
+            this.answers = answers;
         });
     },
+    askDescription(){
+        return this.prompt(questions.description).then(answers => {
+            this.answers = answers;
+        });
+    },
+    askPackageManager(){
+        return this.prompt(questions.packageManager).then(answers => {
+            this.answers = answers;
+        });
+    }
 }
